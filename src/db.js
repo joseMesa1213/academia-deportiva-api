a6ed0fed87c21cb66bc67cd2394b2c79eb333b47
joseMesa1213 <jose.mesar@udea.edu.co>
2026-04-14 17:47:15 -0500

    Conexion a Supabase

--- src/db.js
+++ src/db.js
@@ -2,15 +2,12 @@ const { Pool } = require('pg')
 require('dotenv').config()
 
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }
 })
 
 pool.connect()
-  .then(() => console.log('Conectado a PostgreSQL'))
+  .then(() => console.log('Conectado a Supabase PostgreSQL'))
   .catch(err => console.error('Error de conexión:', err))
 
 module.exports = pool
